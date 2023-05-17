bdfc1d134ba972e4375a056958c59e594323c801
matan <48315169+matanbrimer@users.noreply.github.com>
2023-05-17 21:32:02 +0300

    Add files via upload

---
+++ sources/Character.cpp
@@ -0,0 +1,15 @@
+#include "Character.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace ariel;
+
+
+
+Character::Character(/* args */)
+{
+}
+
+Character::~Character()
+{
+}
---
+++ sources/Team2.cpp
@@ -0,0 +1,27 @@
+#include "Team.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace ariel;
+
+
+Team::Team(Character* player)
+{
+}
+
+Team::~Team()
+{
+}
+
+void Team:: add(Character* player){
+
+}
+void Team:: attack(Team* team){
+
+}
+int Team:: stillAlive(){
+    return 0;
+}
+void Team:: print(){
+    
+}
